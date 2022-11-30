f1247ef2ef2379a8656ac9750764b7f94b60c0ef
Maximilian Wittenburg <max.wittenburg@gmail.com>
2022-11-30 07:50:29 +0100

    fix concurrent map writes for memoryLock

--- src/go/workitemLock/memoryLock.go
+++ src/go/workitemLock/memoryLock.go
@@ -3,12 +3,14 @@ package workitemLock
 import (
 	"context"
 	"errors"
+	"sync"
 	"time"
 )
 
 // MemoryWorkItemLock is a simple in-memory implementation of the WorkItemLock interface. Cannot be used for horizontal scaling.
 type MemoryWorkItemLock struct {
 	locks map[string]time.Time
+	lock  sync.RWMutex
 }
 
 func NewMemoryWorkItemLock() *MemoryWorkItemLock {
@@ -22,6 +24,9 @@ func NewMemoryWorkItemLock() *MemoryWorkItemLock {
 }
 
 func (w *MemoryWorkItemLock) housekeeping() {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	
 	for workitemID, expiresAt := range w.locks {
 		if time.Now().After(expiresAt) {
 			delete(w.locks, workitemID)
@@ -31,6 +36,9 @@ func (w *MemoryWorkItemLock) housekeeping() {
 }
 
 func (w *MemoryWorkItemLock) Lock(_ context.Context, workitemID string, ttl *time.Duration) error {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
 	if lock, ok := w.locks[workitemID]; ok {
 		if time.Now().Before(lock) {
 			return errors.New("workitem is already locked")
@@ -47,6 +55,9 @@ func (w *MemoryWorkItemLock) Lock(_ context.Context, workitemID string, ttl *tim
 }
 
 func (w *MemoryWorkItemLock) Unlock(_ context.Context, workitemID string) error {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
 	delete(w.locks, workitemID)
 	return nil
 }
